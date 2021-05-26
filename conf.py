ebebbde812100d8c4b88ed85f97f50873f253aee
Andréa P Nicolau <35314996+apuzzi@users.noreply.github.com>
2021-05-26 10:24:07 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = "pydata_sphinx_theme"
 
 # Add SIG logo
-html_logo = "_static/sig-nal.jpg"
+html_logo = "_static/signal-logo-notag.jpg"
 
 # Add links to social media
 html_theme_options = {
